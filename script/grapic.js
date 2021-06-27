ba5e3c18c83039a29bfb9a00127dd9d72bb23d43
Shimanshu yadav <shimanshuyadav83@gmail.com>
2021-06-27 19:45:57 +0530

    added shadow with background change and score live level

---
+++ script/grapic.js
@@ -0,0 +1,17 @@
+// create a bg
+const bg_img = new Image()
+bg_img.src = "/imgs/dheet.jpg"
+const paddle_img = new Image();
+paddle_img.src = "/imgs/paddlepx.png";
+const ball_img = new Image();
+ball_img.src = "/imgs/ballpx.png";
+
+
+const LEVEL_IMG = new Image();
+LEVEL_IMG.src = "imgs/level.png";
+
+const LIFE_IMG = new Image();
+LIFE_IMG.src = "imgs/life.png";
+
+const SCORE_IMG = new Image();
+SCORE_IMG.src = "imgs/score.png";
